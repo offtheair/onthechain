439f6aa5285282b2f540be7f35a87441ace38355
nickadamson <naburner@protonmail.com>
2022-11-01 19:06:33 -0700

    fix(eslint): fix all warnings from eslint

--- apps/tv/app/page.tsx
+++ apps/tv/app/page.tsx
@@ -1,9 +1,8 @@
 import Image from 'next/image'
+import { Button } from 'ui'
 import styles from './page.module.css'
 
-import { Button } from 'ui'
-
-export default function Home() {
+export function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
