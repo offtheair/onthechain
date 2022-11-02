37ed20abd3bc738661342d770349aa607b56f8e2
nickadamson <naburner@protonmail.com>
2022-11-01 19:03:35 -0700

    chore(eslint): fix eslint across monorepo

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -5,17 +5,4 @@ module.exports = {
     tsconfigRootDir: __dirname,
     project: ['tsconfig.json'],
   },
-  rules: {
-    'react/button-has-type': 'warn',
-    'react/function-component-definition': [
-      2,
-      { namedComponents: 'arrow-function' },
-    ],
-    'arrow-body-style': 'warn',
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      { varsIgnorePattern: 'React' },
-    ],
-  },
 }
