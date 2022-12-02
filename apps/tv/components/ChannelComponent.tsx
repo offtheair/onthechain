40a12c064f90de25c4886a3874d9a534feffaf8f
nickadamson <naburner@protonmail.com>
2022-12-02 13:42:53 -0800

    chore: scaffold channel component for threejs

---
+++ apps/tv/components/ChannelComponent.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { Suspense } from 'react'
+
+export function ChannelComponent({ curatedMedia }: { curatedMedia: string[] }) {
+  // threeJS
+
+  return (
+    <div className=''>
+      <Suspense>{/* threeJS */}</Suspense>
+    </div>
+  )
+}
