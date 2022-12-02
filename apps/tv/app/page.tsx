e827f98d0887b0cfec613beb2b8622cab1206b98
nickadamson <naburner@protonmail.com>
2022-12-02 14:16:15 -0800

    feat: link to channel page on landing page

--- apps/tv/app/page.tsx
+++ apps/tv/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { Button } from 'ui'
 import styles from './page.module.css'
 
@@ -5,7 +6,9 @@ const Home = () => (
   <div className={styles.container}>
     <main className={styles.main}>
       <h1 className={styles.title}>Welcome to onthechain.tv!</h1>
-      <Button />
+      <Link href='/channel'>
+        <Button />
+      </Link>
     </main>
   </div>
 )
