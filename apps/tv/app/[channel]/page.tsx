ea65c2ccc1755a2ec75e3005bbae2e1025321dd6
nickadamson <naburner@protonmail.com>
2022-12-02 13:42:06 -0800

    chore: scaffold channel dynamic page route

---
+++ apps/tv/app/[channel]/page.tsx
@@ -0,0 +1,22 @@
+import { Suspense } from 'react'
+import { ChannelComponent } from '../../components/ChannelComponent'
+
+export default async function ChannelPage({
+  params,
+}: {
+  params: { channel: string }
+}) {
+  // fake fetch urls
+  await new Promise(() => {})
+
+  const curatedMedia = [
+    'https://ipfs.io/ipfs/bafybeibjz7p2sbp2s7pyt6gu5ngf3dlwx3kc4d2wgpozq34oqpmckhioqq',
+    'https://zora-prod.mypinata.cloud/ipfs/bafybeiddgjqghkr5dgkekywbalv4qqa7oozu5xm4fsfeudydcodm6ol45i',
+  ]
+
+  return (
+    <Suspense fallback='...'>
+      <ChannelComponent curatedMedia={curatedMedia} />
+    </Suspense>
+  )
+}
